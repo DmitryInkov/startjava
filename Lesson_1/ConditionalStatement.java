187e44c5842d41f1b8e8f079e7c18052d29731c0
DmitryInkov <dima_inkov@list.ru>
2020-06-11 13:59:55 +0500

    implement HW_1

---
+++ Lesson_1/ConditionalStatement.java
@@ -0,0 +1,33 @@
+public class ConditionalStatement {
+	public static void main(String[] args) {
+	    int age = 22;
+	    String sex = "man";
+	    double height = 1.82;
+	    char firstLetter = 'M';
+	    if (age > 20) {
+	    	System.out.println("age over 20");
+	    }
+	    if (sex == "man") {
+	    	System.out.println("sex - man");
+	    }
+	    if (sex != "man") {
+	    	System.out.println("sex - woman");
+	    }
+	    if (height < 1.8) {
+	    	System.out.println("growth less than 1.8");
+	    }
+	    else {
+	    	System.out.println("growth greatr than or equal to 1.8");
+	    }
+	    if (firstLetter == 'M') {
+	    	System.out.println("first letter M");
+	    }
+	    else if (firstLetter == 'I') {
+	    	System.out.println("first lettr I");
+	    }
+	    else {
+	    	System.out.println("the first letter is noy M and I");
+	    }
+
+	}
+}
