23573664b13f5de7b6b273cc35090477442a0c23
DmitryInkov <65999806+DmitryInkov@users.noreply.github.com>
2020-06-22 13:21:00 +0500

    Update MyFirstGame.java

--- Lesson_1/MyFirstGame.java
+++ Lesson_1/MyFirstGame.java
@@ -1,14 +1,19 @@
 public class MyFirstGame {
 	public static void main(String[] args) {
 		int guessedNumber = 48;
-		for (int i = 0; i <= 100; i++) {
-			if (i < guessedNumber) {
-				System.out.println("Введенное вами число меньше того, что загадал компьютер");
-			} else if (i > guessedNumber) {
-				System.out.println("Введенное вами число больше того, что загадал компьютер");
-			} else if (i == guessedNumber) {
-				System.out.println("Вы угадали!");
+		int yourNumber = 1;
+		if (yourNumber > 0 && yourNumber <= 100) {
+			while (yourNumber != guessedNumber) {
+				if (yourNumber < guessedNumber) {
+					System.out.println("Введенное вами число меньше того, что загадал компьютер");
+				} else if (yourNumber > guessedNumber) {
+					System.out.println("Введенное вами число больше того, что загадал компьютер");
+				}
+				yourNumber++;
 			}
+			if (yourNumber == guessedNumber) {
+				System.out.println("Вы угадали!");
+			} 
 		}
 	}
 }
