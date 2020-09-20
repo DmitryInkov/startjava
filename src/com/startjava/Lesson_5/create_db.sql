a0ad2206249aff63b5d5905656458f25f77a8837
DmitryInkov <dima_inkov@list.ru>
2020-09-20 21:35:06 +0500

    implement HW5

---
+++ src/com/startjava/Lesson_5/create_db.sql
@@ -0,0 +1,12 @@
+CREATE DATABASE Jaegers;
+
+CREATE TABLE Jaegers(
+    id SERIAL PRIMARY KEY,
+    modelName TEXT,
+    mark TEXT, 
+    height INTEGER, 
+    weight INTEGER, 
+    status TEXT, 
+    origin TEXT, 
+    launch INTEGER, 
+    kaijuKill INTEGER);
